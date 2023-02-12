ea81dd150d0e92557d211a19b9034dc228d1cede
potakaaa <111859181+potakaaa@users.noreply.github.com>
2023-02-12 18:54:42 +0800

    Update JiCashMethods.java

--- JiCashMethods.java
+++ JiCashMethods.java
@@ -7,35 +7,67 @@ public class JiCashMethods {
 
     double SendMoney(double currentBalance, double addedMoney, String moneyReceiver) {
         currentBalance = currentBalance - addedMoney;
-        if (currentBalance < addedMoney) {
-            System.out.println("ERROR");
-        } else {
-            System.out.println("Total: " + addedMoney);
-            System.out.println("Money sent to: " + moneyReceiver);
-        }
         return currentBalance;
     }
 
     double BillsPayment(double currentBalance, double paidMoney, String billerName) {
         currentBalance = currentBalance - paidMoney;
-        if (currentBalance < paidMoney) {
-            System.out.println("ERROR");
-        } else {
-            System.out.println("Total: " + paidMoney);
-            System.out.println("Paid to: " + billerName);
-        }
         return currentBalance;
     }
 
     double BankTransfer(double currentBalance, double paidMoney, String bankName) {
         currentBalance = currentBalance - paidMoney;
-        if (currentBalance < paidMoney) {
-            System.out.println("ERROR");
-        } else {
-            System.out.println("Total: " + paidMoney);
-            System.out.println("Bank Name: " + bankName);
-        }
         return currentBalance;
     }
+
+    void LoadingAnim(String startingWord) {
+        System.out.print(startingWord);
+        for (int i = 0; i < 3; i++) {
+            try {
+                Thread.sleep(500);
+                System.out.print(".");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    // ERROR CODES
+    void ErrorCode01(String errorName) {
+        System.out.println();
+        System.out.println("ERROR 01: Code is Invalid!");
+        System.out.println("Error found on " + errorName);
+        System.out.println();
+    }
+    void ErrorCode02(String errorName) {
+        System.out.println();
+        System.out.println("ERROR 02: Failed to create connection!");
+        System.out.println("Error found on " + errorName);
+        System.out.println();
+    }
+    void ErrorCode03(String errorName) {
+        System.out.println();
+        System.out.println("ERROR 03: Input should only contain alphabetical characters!");
+        System.out.println("Error found on " + errorName);
+        System.out.println();
+    }
+    void ErrorCode04(String errorName) {
+        System.out.println();
+        System.out.println("ERROR 04: Input should only contain numerical characters!");
+        System.out.println("Error found on " + errorName);
+        System.out.println();
+    }
+    void ErrorCode05(String errorName) {
+        System.out.println();
+        System.out.println("ERROR 05: Input should only contain 11 numerical characters");
+        System.out.println("Error found on " + errorName);
+        System.out.println();
+    }
+    void ErrorCode06(String errorName) {
+        System.out.println();
+        System.out.println("ERROR 06: Input should only contain 6 numerical characters");
+        System.out.println("Error found on " + errorName);
+        System.out.println();
+    }
+
 
 }
